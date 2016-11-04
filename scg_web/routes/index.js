093a344dd1641b6119f40d083c8e3a1170867ec0
haracejacob <dkrahd12@gmail.com>
2016-11-04 11:53:37 +0900

    dsdasdas

--- scg_web/routes/index.js
+++ scg_web/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config/config.json')[process.env.NODE_ENV || "development"];
+
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var today = moment().format("YYYY-MM-DD");
+  var now = moment().format("YYYY-MM-DD HH:mm:ss");
   res.render('index', { title: 'Express' });
 });
 
